a34b485a3499721054da163af4e91e819ef1c960
DESKTOP-LK1K1SG\USUARIO <marciomartinsvred@gmail.com>
2023-06-28 19:09:51 -0300

    Initial commit

---
+++ src/Domain/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace IWantApp.Domain.Products;
+
+public abstract class Product : Entity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Guid CategoryId { get; set; }
+    public string Category { get; set; }
+    public bool HasStock { get; set; }
+
+}
